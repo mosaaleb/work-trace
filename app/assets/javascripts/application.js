0964890349b684d827b53f77956a50c6a8a9ead9
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-26 17:22:59 +0200

    Add basic nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap-sprockets
